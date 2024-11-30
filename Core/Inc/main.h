7c89d4dceb8887fad7a974f6e64d3c1deb9a0195
sunyuan1111 <17303347797@163.com>
2024-11-30 22:05:06 +0800

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,82 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_C13_Pin GPIO_PIN_13
+#define LED_C13_GPIO_Port GPIOC
+#define LED_A0_Pin GPIO_PIN_0
+#define LED_A0_GPIO_Port GPIOA
+#define LED_A1_Pin GPIO_PIN_1
+#define LED_A1_GPIO_Port GPIOA
+#define LED_A2_Pin GPIO_PIN_2
+#define LED_A2_GPIO_Port GPIOA
+#define LED_B0_Pin GPIO_PIN_0
+#define LED_B0_GPIO_Port GPIOB
+#define SWITCH_Pin GPIO_PIN_5
+#define SWITCH_GPIO_Port GPIOB
+#define SWITCH_EXTI_IRQn EXTI9_5_IRQn
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
